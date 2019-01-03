d4f0527c1f37bcdf78c40d98bf41b763e06d38ce
Cassio Godinho <cassiopgodinho@gmail.com>
2019-01-03 21:30:22 -0200

    skip json routes

--- lib/web/main/routes/clients.rb
+++ lib/web/main/routes/clients.rb
@@ -6,17 +6,11 @@ module Web
   module Main
     module Routes
       class Clients < Roda
-        plugin :type_routing
         plugin :empty_root
         route do |r|
           r.root do
             @clients = ::Main::Schemas::Clients.all_clients
-            r.html do
-              Web::Main::Views::Clients.new(clients: @clients).call :index
-            end
-            r.json do
-              Web::Main::Serializers::Clients.new(@clients).index
-            end
+            Web::Main::Views::Clients.new(clients: @clients).call :index
           end
         end
       end
