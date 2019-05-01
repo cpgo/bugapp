84c24e52d62eaa1542e56f7eeee4a2cfc32020ea
Cassio Godinho <cassiopgodinho@gmail.com>
2019-05-01 16:43:26 -0300

    append slash

--- config/boot.rb
+++ config/boot.rb
@@ -5,6 +5,7 @@ require "pry" if ENV["RACK_ENV"] != "production"
 
 class App < Roda
   plugin :multi_run
+  plugin :run_append_slash
   run "clients", Routes::Clients
   route do |r|
     r.multi_run
